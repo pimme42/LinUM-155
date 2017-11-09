7fb78a1c2a98cede3ede32950213eeca35575b2b
Tobias Rörstam <github@pimme.org>
2017-11-09 18:27:25 +0100

    Libpower.c skapad

---
+++ libpower/libpower.c
@@ -0,0 +1,11 @@
+#include "libpower.h"
+
+float calc_power_r(float volt, float resistance)
+{
+  return volt * volt / resistance;
+}
+
+float calc_power_i(float volt, float current)
+{
+  return volt * current;
+}
