ffbccb2e02e2eeeafa0b6d8b462fb4d64cc57acd
SiggeSiege <simon_gronberg@hotmail.com>
2017-11-12 11:14:15 +0100

    Added documentation according to doxygen standard

--- libresistance/calc_resistance.h
+++ libresistance/calc_resistance.h
@@ -1,6 +1,21 @@
-#ifndef LIBRESISTANCE
-#define LIBRESISTANCE
+/**@file 
+ * @brief The function calc_resistance for calculating equivalent resistance
+ * @author Simon Groenberg
+ * @bug No known bugs
+ */
 
-float calc_resistance(int count, char conn, float *array);
+#ifndef CALC_RESISTANCE
+#define CALC_RESISTANCE
+
+/**
+ * @brief This function calculates the equivalent resistance of resistor
+ * networks
+ * @param count The number of connected resistors
+ * @param conn The type of connection, 'S' for serial or 'P' for parallel
+ * @param array a pointer to an array that keeps the resistance of each
+ * connected resistor
+ * @return The equivalent resistance
+ */
+float calc_resistance(int count, char conn, float* array);
 
 #endif
