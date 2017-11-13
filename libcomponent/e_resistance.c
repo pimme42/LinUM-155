8d709996c098d63f12cda2eb2435fcb36bf8a3ec
shot87 <akhalatyan@gmail.com>
2017-11-13 04:19:22 +0100

    added %5 error check (not working yet)

--- libcomponent/e_resistance.c
+++ libcomponent/e_resistance.c
@@ -19,13 +19,25 @@ e12[8]=47;
 e12[9]=56;
 e12[10]=68;
 e12[11]=82;
- 
+
 int len=32;
+
+
+/*
+tmp array: elements 0,1,2 are resistors that sum up to orig_resistance+-2%, element 3 is the alternative ans if exact match is not found, element 4 is the error for every new approximated matc
+*/
+
+int *tmp=(int*)malloc(5*sizeof(int)); 
+
+for (int j=0; j<4; j++){	//initializing approximate values to 0s
+	tmp[j]=0;
+}
+
+tmp[4]==orig_resistance*5/100; //Error tolerance 5%
 
 for (int i=0; i< len-12; i++){
 
 	e12[i+12]=e12[i]*10;
-	
 }
 
 res_array[0]=0;
@@ -61,9 +73,36 @@ res_array[2]=0;
                                         res_array[2]=e12[k];
 					ans=3;
 					i,j,k=-1;
+				}else if (abs(orig_resistance-e12[i])<=tmp[4]){
+					tmp[0]=e12[i];
+					tmp[1]=0;
+					tmp[2]=0;
+					tmp[3]=1;
+					tmp[4]=abs(orig_resistance-e12[i]);
+				}else if (abs(orig_resistance-e12[i]-e12[j])<=tmp[4]){
+					tmp[0]=e12[i];
+					tmp[1]=e12[j];
+					tmp[2]=0;
+					tmp[3]=2;
+					tmp[4]=abs(orig_resistance-e12[i]-e12[j]);
+				}else if (abs(orig_resistance-e12[i]-e12[j]-e12[k])<=tmp[4]){
+					tmp[0]=e12[i];
+					tmp[1]=e12[j];
+					tmp[2]=e12[k];
+					tmp[3]=3;
+					tmp[4]=abs(orig_resistance-e12[i]-e12[j]-e12[k]);
 				}
 			}
 		}
 	}
+
+
+if(ans==0 && tmp[3]!=0){
+	for(int l=0; l<3; l++){
+		res_array[l]=tmp[l];
+	}
+ans=tmp[3];
+}
+
 return ans;
 }
